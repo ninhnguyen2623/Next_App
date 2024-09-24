949a983dab3bcfea12bb7b2b493660dc74d36d9d
ninhnguyen2623 <ninh1160@gmail.com>
2024-09-24 15:18:33 +0700

    hello world

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Hello world Ninh dev 
+          Hello world Ninh dev learn next js
         </p>
         <div>
           <a
